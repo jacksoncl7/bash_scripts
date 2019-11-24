983cc772bcea4ca05d9392cc0d43a15105250eac
jacksoncl7 <jacksonlima@id.uff.br>
2019-11-23 23:01:37 -0300

    Adjusting script desc

--- sispro_module_test.sh
+++ sispro_module_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# first argument is system dir
-# second argument is pattern name 
+# first  argument is the system dir
+# second argument is the pattern 
 
 path="$1/spec/models/*$2* $1/spec/presenters/$2* $1/spec/validators/$2*/ \
       $1/spec/controllers/*$2* $1/spec/decorators/$2* $1/spec/mailers/$2* \
